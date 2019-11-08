1ff77a64164add1a7da59a4e5e5b0ba22717ea7c
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-11-08 16:45:17 -0300

    Android fundamentals 01.2 Part A: Your first interactive UI

---
+++ Android fundamentals 01.2 Part A - Your first interactive UI/HelloToast/app/src/main/java/pissuti/nathalia/hellotoast/MainActivity.java
@@ -0,0 +1,26 @@
+package pissuti.nathalia.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+
+    }
+
+}
