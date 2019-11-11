0f639ad1b0ce37fffbdcbbdd12a6c6846fe7235b
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-11-11 14:32:28 -0300

    Android fundamentals 01.3: Text and scrolling views

---
+++ Android fundamentals 01.3 - Text and scrolling views/ScrollingTextChallenge/app/src/main/java/pissuti/nathalia/scrollingtextchallenge/MainActivity.java
@@ -0,0 +1,18 @@
+package pissuti.nathalia.scrollingtextchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addComment(View view) {
+    }
+}
