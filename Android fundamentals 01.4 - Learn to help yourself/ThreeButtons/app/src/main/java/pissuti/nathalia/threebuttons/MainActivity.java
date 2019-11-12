a90504f2c17ebabb92f2a2eb4a9ae510c6cbad28
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-11-12 09:17:23 -0300

    Android fundamentals 02.1: Activities and intents

--- Android fundamentals 01.4 - Learn to help yourself/ThreeButtons/app/src/main/java/pissuti/nathalia/threebuttons/MainActivity.java
+++ Android fundamentals 01.4 - Learn to help yourself/ThreeButtons/app/src/main/java/pissuti/nathalia/threebuttons/MainActivity.java
@@ -2,10 +2,14 @@ package pissuti.nathalia.threebuttons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_PHRASE = "pissuti.nathalia.threebuttons.extra.PHRASE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showText(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
 
+        switch(view.getId()) {
+            case R.id.button_1:
+                intent.putExtra(EXTRA_PHRASE, "Frase 1");
+                break;
+            case R.id.button_2:
+                intent.putExtra(EXTRA_PHRASE, "Frase 2");
+                break;
+            case R.id.button_3:
+                intent.putExtra(EXTRA_PHRASE, "Frase 3");
+                break;
+        }
+
+        startActivity(intent);
     }
 }
--- Android fundamentals 01.4 - Learn to help yourself/ThreeButtons/app/src/main/java/pissuti/nathalia/threebuttons/SecondActivity.java
+++ Android fundamentals 01.4 - Learn to help yourself/ThreeButtons/app/src/main/java/pissuti/nathalia/threebuttons/SecondActivity.java
@@ -2,7 +2,9 @@ package pissuti.nathalia.threebuttons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String phrase = intent.getStringExtra(MainActivity.EXTRA_PHRASE);
+
+        TextView textViewPhrase = findViewById(R.id.phrase);
+        textViewPhrase.setText(phrase);
     }
 }
