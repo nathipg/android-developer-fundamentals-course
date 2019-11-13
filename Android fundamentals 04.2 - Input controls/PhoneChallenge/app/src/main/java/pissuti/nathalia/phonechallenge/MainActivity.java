70dc6a8856f5e2f8f4dec08464b127e2d1fd6a1f
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-11-13 16:02:21 -0300

    Android fundamentals 04.2: Input controls

---
+++ Android fundamentals 04.2 - Input controls/PhoneChallenge/app/src/main/java/pissuti/nathalia/phonechallenge/MainActivity.java
@@ -0,0 +1,57 @@
+package pissuti.nathalia.phonechallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.phone);
+        if (editText != null)
+            editText.setOnEditorActionListener
+                    (new TextView.OnEditorActionListener() {
+                        @Override
+                        public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                            boolean handled = false;
+                            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                                dialNumber();
+                                handled = true;
+                            }
+                            return handled;
+                        }
+                    });
+    }
+
+    private void dialNumber() {
+        EditText editText = findViewById(R.id.phone);
+        String phoneNum = null;
+
+        if (editText != null) phoneNum = "tel:" +
+                editText.getText().toString();
+
+        Log.d(LOG_TAG, "dialNumber: " + phoneNum);
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(phoneNum));
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+    }
+}
