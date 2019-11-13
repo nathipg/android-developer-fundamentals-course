0b5cfdc863c3018ac43af52ffe88d939f7442389
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-11-13 13:40:11 -0300

    Android fundamentals 04.1: Clickable images

---
+++ Android fundamentals 04.1 - Clickable images/DroidCafe/app/src/main/java/pissuti/nathalia/droidcafe/OrderActivity.java
@@ -0,0 +1,28 @@
+package pissuti.nathalia.droidcafe;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        if(message != null) {
+            TextView textView = findViewById(R.id.order_textview);
+            textView.setText(message);
+        }
+    }
+
+}
