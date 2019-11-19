9c47c92817d65f64e347fdde53a3822037585ed5
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-11-19 16:46:38 -0300

    Android fundamentals 07.2 - AsyncTask and AsyncTaskLoader

---
+++ Android fundamentals 07.2 - AsyncTask and AsyncTaskLoader/WhoWroteItLoader/app/src/main/java/pissuti/nathalia/whowroteitloader/BookLoader.java
@@ -0,0 +1,29 @@
+package pissuti.nathalia.whowroteitloader;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+    }
+
+    public BookLoader(@NonNull Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+}
---
+++ Android fundamentals 07.2 - AsyncTask and AsyncTaskLoader/WhoWroteItLoader/app/src/main/java/pissuti/nathalia/whowroteitloader/MainActivity.java
@@ -0,0 +1,73 @@
+package pissuti.nathalia.whowroteitloader;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBookInput = (EditText)findViewById(R.id.bookInput);
+        mTitleText = (TextView)findViewById(R.id.titleText);
+        mAuthorText = (TextView)findViewById(R.id.authorText);
+    }
+
+    public void searchBooks(View view) {
+        String queryString = mBookInput.getText().toString();
+
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputManager != null ) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
+}
