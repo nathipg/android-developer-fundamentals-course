c6031461ba68d0184be182e263a696dacacb80f8
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-11-19 14:01:58 -0300

    Android fundamentals 06.1: Espresso for UI testing

---
+++ Android fundamentals 06.1 - Espresso for UI testing/TwoActivities/app/src/androidTest/java/pissuti/nathalia/twoactivities/ActivityInputOutputTest.java
@@ -0,0 +1,56 @@
+package pissuti.nathalia.twoactivities;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ActivityInputOutputTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("pissuti.nathalia.twoactivities", appContext.getPackageName());
+    }
+
+    @Test
+    public void activityLauch() {
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_second)).perform(click());
+        onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textInputOutput() {
+        onView(withId(R.id.editText_main)).perform(typeText("This is a test."));
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_message)).check(matches(withText("This is a test.")));
+    }
+}
